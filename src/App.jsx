3f9cc9de5bb15e225c26dfb2f012fa591d8aad6f
Library Laptop <laptop@NUL-LENDLAP47.library.northwestern.edu>
2025-07-22 14:33:03 -0500

    first onboarding page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Onboarding from './screens/Onboarding';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <h1>AllState Project</h1>
-      </div>
-    </>
-  )
+  return <Onboarding />;
 }
 
-export default App
+export default App;
